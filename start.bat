f5d8cdfba6bca99442c5e8ec2d444fe9051477e6
haczmrh <haczmrh@gmail.com>
2025-06-30 23:40:54 +0800

    权限

--- start.bat
+++ start.bat
@@ -2,6 +2,14 @@
 REM 进入脚本所在目录
 cd /d %~dp0
 
+REM 检查管理员权限并提权
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+    exit /b
+)
+
 REM 激活虚拟环境
 call venv\Scripts\activate
 
